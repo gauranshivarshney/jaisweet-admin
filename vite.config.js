badf9821e132804d685c94b1b028481ad040d759
TechAce <93094408+gauranshivarshney@users.noreply.github.com>
2025-08-02 15:12:00 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()], 
-  base: '/jaisweet-admin',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  base: '/jaisweet-admin'
 })
